5d1552d3a04a9a1fb75bec1c575e04e32b79df0e
OmerAkcali <omerakcali99@gmail.com>
2022-06-12 16:43:20 +0300

    Init

---
+++ MicroService/Controllers/MainController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroService.Controllers{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase{
+        private static readonly string[] Summaries = new[]{
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger){
+            _logger = logger;
+        }
+
+        
+    }
+}
