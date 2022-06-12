64979c4707732120819711ae4ddc59272c405c8b
OmerAkcali <omerakcali99@gmail.com>
2022-06-12 17:22:52 +0300

    ML Modeli eklendi ve Image Prediction metodu yazıldı

--- MicroService/Controllers/MainController.cs
+++ MicroService/Controllers/MainController.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
 
 namespace MicroService.Controllers{
     [ApiController]
     [Route("[controller]")]
     public class MainController : ControllerBase{
-        private static readonly string[] Summaries = new[]{
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
         private readonly ILogger<MainController> _logger;
 
@@ -19,6 +22,24 @@ namespace MicroService.Controllers{
             _logger = logger;
         }
 
-        
+        [HttpPost("image"), DisableRequestSizeLimit]
+        public async Task<string> GetPrediction([FromForm] IFormFile image){
+            using (var stream = new MemoryStream()){
+                await image.CopyToAsync(stream);
+                Bitmap bmp = new Bitmap(stream);
+
+                var newB = new Bitmap(150, 150, PixelFormat.Format24bppRgb);
+                using (Graphics gr = Graphics.FromImage(newB)){
+                    gr.DrawImage(bmp, new Rectangle(0, 0, newB.Width, newB.Height));
+                }
+
+                bmp = newB;
+
+                var result = ImagePredictor.PredictImage(bmp);
+
+
+                return await Task.FromResult(result);
+            }
+        }
     }
 }
---
+++ MicroService/ImagePredictor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace MicroService{
+    public class ImagePredictor{
+
+        public static string PredictImage(Bitmap image){
+            Tensor<float> input = new DenseTensor<float> (new [] { 1,150, 150,3 });
+            for (int y = 0; y < 150; y++)
+            {
+                for (int x = 0; x < 150; x++){
+                    input[0,y, x, 0] = image.GetPixel(x, y).R;
+                    input[0,y, x, 1] = image.GetPixel(x, y).G;
+                    input[0,y, x, 2] = image.GetPixel(x, y).B;
+                }
+            }
+            
+            
+            var inputs = new List<NamedOnnxValue> {
+                NamedOnnxValue.CreateFromTensor("input", input)
+            };
+
+            using var session = new InferenceSession( Path.Combine(Directory.GetCurrentDirectory(),"Model","model_son.onnx"));
+            var run =session.Run(inputs);
+
+            foreach (var i in run.First().AsTensor<float>().ToArray()){
+                Console.WriteLine(i);
+            }
+
+            var array = run.First().AsTensor<float>().ToArray();
+            int index = 0;
+            for (int i = 0; i < array.Length; i++){
+                if (array[i] > array[index]) index = i;
+            }
+            var labels = System.IO.File.ReadLines(Path.Combine(Directory.GetCurrentDirectory(),"Model", "labels.txt"));
+            return labels.ToList()[index];
+        }
+        
+    }
+}
